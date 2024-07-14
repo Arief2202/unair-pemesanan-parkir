d54b35d50e284c501341ca6c2da7c49fe5cfc1b9
Arief <arief.d2202@gmail.com>
2024-07-15 00:30:43 +0700

    repair

--- index.php
+++ index.php
@@ -1,13 +1,2 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Smart Parking | Login</title>
-    <link href="/bootstrap-5.3.3/bootstrap.min.css" rel="stylesheet">
-  </head>
-  <body>
-    
-    <script src="/bootstrap-5.3.3/bootstrap.bundle.min.js"></script>
-  </body>
-</html>
+<?php
+  header('Location: /login.php');
