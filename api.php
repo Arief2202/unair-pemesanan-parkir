b70248fcab82f6976ac923b83c4bb3b932b6f762
Arief <arief.d2202@gmail.com>
2024-07-20 15:06:42 +0700

    test

--- api.php
+++ api.php
@@ -35,13 +35,15 @@ if (isset($_GET['checkin']) && isset($_GET['id_card'])) {
     $pemesanan = mysqli_fetch_object(mysqli_query($conn, "SELECT * FROM pemesanan WHERE user_id = '" . $user->id . "' AND status = 1"));
     if ($user) {
         if ($pemesanan) {
+            http_response_code(401);
             $slots = [["r3", 201], ["r3", 202], ["r3", 203], ["r3", 204], ["r3", 205], ["r3", 206]];
             for ($a = 0; $a < 6; $a++) {
                 if ($pemesanan->slot == $slots[$a][0]) {
-                    http_response_code($slots[$a][1]);
+                    http_response_code(402);
                     
                     $result = mysqli_query($conn, "UPDATE `pemesanan` SET `status` = '2' WHERE `pemesanan`.`id` = " . $pemesanan->id . ";");
                     if ($result) {
+                            http_response_code($slots[$a][1]);
                             echo json_encode([
                                 "status" => "Success",
                                 "pesan" => "Checkin Success",
